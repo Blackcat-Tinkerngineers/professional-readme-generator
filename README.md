3eda34783854b21b364ea9de6a7790aeadec2ec1
Chelsie L Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-01-16 18:02:08 -1000

    final commit before turning this assignment in

--- README.md
+++ README.md
@@ -1,5 +1,53 @@
-# Professional README Generator Starter Code
+# Professional-README-Generator
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
 
-[How to create a Professional README](./readme-guide.md)
 
- 
+## Description
+
+This project will generate a README.md file via your command line. Fancy that. 
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+npm i
+```
+
+## Usage
+
+It's really fun
+
+## License
+
+This project is licensed under the MIT license.
+  
+## Contributing
+
+n/a
+
+## Tests
+
+To run tests, run the following command:
+
+```
+npm test
+```
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at hithere@gmail.com. You can find more of my work at [chelsielbgonsalves](https://github.com/chelsielbgonsalves/).
--- input.js
+++ input.js
@@ -1,87 +1,129 @@
-var inquirer = require('inquirer');
-inquirer
-  .prompt([
-      {
-    name: 'project.title',
-    type: 'confirm',
-    message: 'Are you ready to start on your README.md?',
-    choices: ['yes', 'no'],
-      },
-      validate((confirm) => {
-       
-        var done = this.async();
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-])  
- .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
+}
 
-    var prompts = new Rx.Subject();
-    inquirer.prompt(prompts);
-
-    inquirer
-    .prompt([
-        {
-        name: 'project.title',
-        type: 'input',
-        message: 'What is the name of your project?',
-    },    
-    
-    {
-        name: 'project.description',
-        type: 'input',
-        message: 'Enter a description of the project',
-        
-    },
-    {
-        name: 'install.instructions',
-        type: 'input',
-        message: 'Enter installation instructions',
-        
-
-    },
-    {
-        name: 'usage.info',
-        type: 'input',
-        message: 'Enter usage information',
-        
-    },
-    {
-        name: 'contribute.guidelines',
-        type: 'input',
-        message: 'Enter any contribution guidelines',
-        
-    },
-    {
-        name: 'test.instructions',
-        type: 'input',
-        message: 'Enter test instructions',
-        
-    },
-    {
-        name: 'project.icense',
-        type: 'list',
-        message: 'Please choose a license for this application',
-        
-    },
-    {
-        name: 'github.username',
-        type: 'input',
-        message: 'Please enter your GitHub username',
-        
-    },
-    {
-        name: 'user.email',
-        type: 'input',
-        message: 'Enter your email address for contact information',
-        
-    },
-
- 
-]);
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+}
 
 
- });
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+
+This project is licensed under the ${license} license.`;
+  }
+  return '';
+}
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project's name?"
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm i'
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?'
+  }
+]).then(data => {
+  fs.writeFileSync('README.md', 
+`# ${data.title}
+${renderLicenseBadge(data.license)}
+
+
+## Description
+
+${data.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+  
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${
+    data.email
+  }. You can find more of my work at [${data.github}](https://github.com/${data.github}/).`);});
